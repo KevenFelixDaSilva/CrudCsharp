5ed676d8495fe543db045cdd73c484cd77892feb
Keven Félix <keven.05@hotmail.com>
2023-07-11 17:13:00 -0300

    Trabalhando com arquivos de configuração |  db local no codigo

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
 
 app.MapGet("/", () => "Hello World!");
 
@@ -37,15 +39,21 @@ app.MapGet("/GetProductByHeader", (HttpRequest request) => {
     return request.Headers["product-code"].ToString();
 });
 
+app.MapGet("/Configuration/Database", (IConfiguration configuration) => {
+    return Results.Ok($"{configuration["Database:connection"]}/{configuration["Database:Port"]}");
+});
+
 app.Run();
 
 public static class ProductRepository{
-    public static List<Product> Products { get; set; }
+    public static List<Product> Products { get; set; } = new List<Product>();
 
-    public static void Add(Product product){
-        if(Products == null){
-            Products = new List<Product>();   
-        }
+    public static void Init(IConfiguration configuration){
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
+
+    public static void Add(Product product){          
         Products.Add(product);
     }
 
